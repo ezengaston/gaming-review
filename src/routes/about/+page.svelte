23e200749dd38a03e0a8b609d15da7f09538914c
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-18 12:33:58 +0300

    Added about route

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<div class="about">
+  <h2>About us</h2>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse,
+    voluptatibus. Dolorum sint adipisci sed ad commodi, veniam itaque quod
+    maiores officia, obcaecati molestiae. Dolorem eaque totam nobis repellendus
+    illo vero quo esse odit obcaecati debitis earum temporibus dolorum
+    voluptatibus quis saepe dolor eos nemo vitae voluptatem voluptate facilis
+    quidem, ipsum similique. Maiores inventore atque sit eaque molestiae velit
+    quia non!
+  </p>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
