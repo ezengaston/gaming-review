3ab74603fb374af351fe2c9bd3b8418fbb56d261
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-18 16:00:49 +0300

    Added seperate layout for guides

--- src/routes/(main-nav)/guides/+layout@.svelte
+++ src/routes/(main-nav)/guides/+layout@.svelte
@@ -4,12 +4,14 @@
 </script>
 
 <header>
-  <Title title="Ninja Gaming Guides" />
-  <div class="links">
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/guides">Guides</a>
-  </div>
+  <nav>
+    <Title title="Ninja Gaming Guides" />
+    <div class="links">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/guides">Guides</a>
+    </div>
+  </nav>
 </header>
 <main>
   <slot />
@@ -20,23 +22,25 @@
 
 <style>
   header {
-    display: flex;
-    justify-content: center;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.1);
   }
-
   main {
     max-width: 960px;
     margin: 20px auto;
   }
-
   footer {
     text-align: center;
   }
-
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 960px;
+    margin: 0 auto;
+  }
   .links {
     margin-left: auto;
   }
-
   a {
     margin-left: 10px;
   }
