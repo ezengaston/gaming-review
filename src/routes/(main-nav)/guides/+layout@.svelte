62bc48ef29561383fa3765a3f897fbd3a7cc3154
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-18 15:48:43 +0300

    Changed folder structure

---
+++ src/routes/(main-nav)/guides/+layout@.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Title from "$lib/title.svelte";
+  import "../../../styles/global.css";
+</script>
+
+<header>
+  <Title title="Ninja Gaming Guides" />
+  <div class="links">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/guides">Guides</a>
+  </div>
+</header>
+<main>
+  <slot />
+</main>
+<footer>
+  <p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+  }
+
+  main {
+    max-width: 960px;
+    margin: 20px auto;
+  }
+
+  footer {
+    text-align: center;
+  }
+
+  .links {
+    margin-left: auto;
+  }
+
+  a {
+    margin-left: 10px;
+  }
+</style>
