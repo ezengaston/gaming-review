8f79ce8705aab7cd3ac122f7943ba6305d7d81d4
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-18 12:57:37 +0300

    Added index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome</h2>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. In pariatur ipsa
+    neque nobis laudantium laborum hic minus quisquam fugit ipsum quaerat eum
+    magnam corporis excepturi non, dolor officia sapiente iste sequi inventore
+    illum sint? Illum soluta tempore rerum officia voluptate.
+  </p>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
